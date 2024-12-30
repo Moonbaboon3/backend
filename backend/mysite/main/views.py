0d5183b61505bd6efb912fa67b137295d4a405c1
Moonbaboon3 <147054617+Moonbaboon3@users.noreply.github.com>
2024-12-31 01:20:04 +0200

    productCard.html template added
    images are not displayed in product page for some reason, will fix that.

--- backend/mysite/main/views.py
+++ backend/mysite/main/views.py
@@ -1,6 +1,6 @@
-from pyexpat.errors import messages
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
 
@@ -9,27 +9,26 @@ def index_view(request):
     return render(request, 'home.html')
 
 def products_view(request):
-<<<<<<< Updated upstream
-    return render(request, 'products.html')
-def cart_view(request):
-    return render(request, 'cart.html')
-def account_view(request):
-    return render(request, 'account.html')
-def myorders_view(request):
-    return render(request, 'myorders.html')
-def productdetail_view(request):
-    return render(request, 'productdetail.html')
-def signup_view(request):
-    return render(request, 'signup.html')
-def login_view(request):
-    return render(request, 'login.html')
-def visa_view(request):
-    return render(request, 'checkout.html')
-=======
     products = Product.objects.all()
     return render(request, 'products.html',{'products': products})
 
-def productDetails_view(request,pk):
-    product = Product.objects.get(id=pk)
-    return render(request, 'productDetailTab.html', {'product':product})
+def cart_view(request):
+    return render(request, 'cart.html')
 
+def account_view(request):
+    return render(request, 'account.html')
+
+def myorders_view(request):
+    return render(request, 'myorders.html')
+
+def productdetail_view(request):
+    return render(request, 'productdetail.html')
+
+def signup_view(request):
+    return render(request, 'signup.html')
+
+def login_view(request):
+    return render(request, 'login.html')
+
+def visa_view(request):
+    return render(request, 'checkout.html')
