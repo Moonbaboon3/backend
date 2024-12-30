a3fa049ee791a57ccef0c4ac0d86e64e26970abb
Moonbaboon3 <147054617+Moonbaboon3@users.noreply.github.com>
2024-12-30 15:14:57 +0200

    restarting progress
    dsrt

---
+++ backend/README.md
@@ -0,0 +1,5 @@
+# backend
+
+
+Django tutorial 
+https://youtube.com/playlist?list=PLknwEmKsW8OtK_n48UOuYGxJPbSFrICxm&si=Fe6476II7YE4Qvpx
--- my_app/main/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index_view, name='home'),
-    path('products/',views.products_view,name='products' )
-] 
