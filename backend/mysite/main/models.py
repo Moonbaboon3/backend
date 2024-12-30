22a86c6d7108dbfb76f0182228f36e82e10fee83
Moonbaboon3 <147054617+Moonbaboon3@users.noreply.github.com>
2024-12-30 15:42:47 +0200

    added models and altered the products page to be dynamic
    models in model.py file
    added productCard.html template to store the card template and to reuse it

--- backend/mysite/main/models.py
+++ backend/mysite/main/models.py
@@ -1,3 +1,49 @@
 from django.db import models
-
+import datetime
 # Create your models here.
+
+# products category
+class Category(models.Model):
+    ctgry_name = models.CharField(max_length=50)
+
+    def _str_(self):
+        return self.ctgry_name
+
+#customers
+class Customer(models.Model):
+    fName = models.CharField(max_length=50)
+    lName = models.CharField(max_length=50)
+    phone = models.CharField(max_length= 10)
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=100)
+
+    def _str_(self):
+        return f'{self.fName} {self.lName}'
+
+class Product(models.Model):
+    pName = models.CharField(max_length=100)
+    price = models.DecimalField(default=0, decimal_places=2, max_digits=6)
+    ctgry = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
+    description = models.CharField(max_length=250, default='', blank=True,null=True)
+    image = models.ImageField(upload_to='uploads/product/')
+    is_sale = models.BooleanField(default=False)
+    sale_price = models.DecimalField(default=0, decimal_places=2, max_digits=6)
+
+    def _str_(self):
+        return self.pName
+
+# customer orders
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    address = models.CharField(max_length=100, default='',blank=True)
+    phone = models.CharField(max_length=20,default='',blank=True)
+    data = models.DateField(default=datetime.datetime.today)
+    status = models.BooleanField(default=False)
+
+    def _Str_(self):
+        return self.product
+
+
+
--- backend/mysite/main/views.py
+++ backend/mysite/main/views.py
@@ -1,9 +1,13 @@
-
 from django.http import HttpResponse
 from django.shortcuts import render
+from . models import Product
 
 # Create your views here.
 
 
 def index_view(request):
     return render(request, 'index.html')
+
+def products_view(request):
+    products = Product.objects.all()
+    return render(request, 'products.html',{'products': products})
